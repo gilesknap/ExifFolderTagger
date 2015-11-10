9e6231481a85fd9c6444b4ee27b0e7233217b503
gilesknap <giles.knap@diamond.ac.uk>
2015-11-10 07:18:38 +0000

    further tuning of folder compare

--- FolderCompare.py
+++ FolderCompare.py
@@ -14,12 +14,14 @@ from gi.repository import GExiv2
 # for such files
 # (More explanation required)
 
+# exif date string format
+DATE_FORMAT = '%Y:%m:%d %H:%M:%S'
+
 class FolderCompare:
     def __init__(self):
         # expressions for extracting date from file names - for date checking
         self.google_duplicates_expr = re.compile(r'(.*) \(\d*\)(\..*)')
-        self.date_filename_pattern = '%s-%s-%s %s:%s:%s.%s'
-        self.date_filename_pattern_noext = '%s-%s-%s %s:%s:%s'
+        self.date_filename_pattern = '%s:%s:%s %s:%s:%s.%s'
         self.tag_dates = [
             'Exif.Photo.DateTimeOriginal',
             'Exif.Photo.DateTimeDigitized',
@@ -32,10 +34,19 @@ class FolderCompare:
         self.list_names = ['left', 'right']
         self.file_lists = {}
         self.file_duplicates = {}
-        self.mismatch_count = 0
+        self.mismatch_count = {}
+        self.folders_to_exclude = []
         for name in self.list_names:
             self.file_lists[name] = {}
             self.file_duplicates[name] = 0
+            self.mismatch_count[name] = 0
+
+        # control parameters
+        self.extensions_to_ignore = [
+            '.txt', '.pdf',
+        ]
+        self.folders_to_exclude = []
+        self.folders_to_exclude = []
 
     def dictionary_compare(self, first_name, compare_name):
         print("comparing {0} with {1}".format(first_name, compare_name))
@@ -49,20 +60,29 @@ class FolderCompare:
             else:
                 this_count = 0
             if first_count > this_count:   # use > so that mismatches are counted only once since we compare both ways
-                self.mismatch_count += 1
-                for this_full_name,this_date in folder_dates:
-                    print('mismatch on file "{0}". {1} count={2}, {3} count={4}'.format(
-                        this_full_name, first_name, first_count, compare_name, this_count)
-                    )
+                self.mismatch_count[first_name] += 1
+                this_full_name,this_date = folder_dates[0]
+                print('mismatch on index "{4}", {0} count={1}, {2} count={3}'.format(
+                    first_name, first_count, compare_name, this_count, file_name)
+                )
 
     def compare(self, left, right):
+        print("skipping extensions {0}".format(self.extensions_to_ignore))
+        print("skipping folders {0}".format(self.folders_to_exclude))
+        print('using exif = {0}'.format(self.useExif))
         roots = locals()
         for list_name in self.list_names:
             root_folder = os.path.abspath(roots[list_name])
             print("reading files from directory {0}".format(root_folder))
             for dirName, subdirList, fileList in os.walk(root_folder):
                 folder_name = os.path.relpath(dirName, root_folder)
+                if any(folder_name.startswith(s) for s in self.folders_to_exclude):
+                    print("FOLDER {0} Skipped ...".format(dirName))
+                    continue
                 for file_name in fileList:
+                    root, ext = os.path.splitext(file_name)
+                    if ext.lower() in self.extensions_to_ignore:
+                        continue
                     full_name = os.path.join(dirName, file_name)
                     file_date = datetime.fromtimestamp(os.path.getmtime(full_name))
                     # if google uploader gets duplicate names in a month it adds (1) (2) ... so treat these as the same
@@ -74,17 +94,18 @@ class FolderCompare:
                         m = reg.match(file_name)
                         if m:
                             file_name = self.date_filename_pattern % m.groups()
-                            if google:
+                            if google and self.useExif:
                                 # google file name dates are wrong by a few seconds, read the metadata instead for these
                                 metadata = GExiv2.Metadata(full_name)
                                 if metadata:
                                     for date_tag in self.tag_dates:
                                         tag = metadata.get(date_tag, None)
                                         if tag is not None:
-                                            file_date = datetime.strptime(tag, "%Y:%m:%d %H:%M:%S")
-                                            file_name = file_date.strftime(self.date_filename_pattern_noext) \
-                                                + '.' + m.group(6)
-                                            print ('google file {0} converted to {1}'.format(full_name,file_name))
+                                            file_date = datetime.strptime(tag, DATE_FORMAT)
+                                            file_name = file_date.strftime(DATE_FORMAT) \
+                                                + '.' + m.group(7)
+                                            # print ('google file {0} with exif {1} converted to {2}'
+                                            #       .format(full_name, file_date, file_name))
                                             break
 
                     if file_name in self.file_lists[list_name].keys():
@@ -97,11 +118,14 @@ class FolderCompare:
         first_name = self.list_names[0]
         for compare_name in self.list_names[1:]:
             self.dictionary_compare(first_name, compare_name)
+            print('############################################################################################')
+            print('############################################################################################')
             self.dictionary_compare(compare_name, first_name)
 
         for list_name in self.list_names:
-            print('unique files read for {0} = {1}, duplicates = {2}'.format(
-                list_name, len(self.file_lists[list_name]),self.file_duplicates[list_name]))
-        print("mismatches = {0}".format(self.mismatch_count))
+            print('unique files read for {0} = {1}, duplicates = {2}, mismatches = {3}'.format(
+                list_name, len(self.file_lists[list_name]),self.file_duplicates[list_name],
+                self.mismatch_count[list_name]
+            ))
 
 
--- example2.py
+++ example2.py
@@ -13,7 +13,11 @@ def doFolderScan():
         #for i in range(1996,2015):
            # c.compare(r'E:\GoogleDrive\Google Photos\{0}'.format(i), r'E:\Ubuntu\Pictures\{0}'.format(i))
         #     c.compare(r'/mnt/E_DRIVE/GoogleDrive/Google Photos/{0}'.format(i), r'/mnt/E_DRIVE/Ubuntu/Pictures/{0}'.format(i))
-        c.compare(r'/mnt/E_DRIVE/GoogleDrive/Google Photos/2015', r'/mnt/E_DRIVE/Ubuntu/Pictures/2015')
+        #c.compare(r'/mnt/E_DRIVE/GoogleDrive/Google Photos/2015', r'/mnt/E_DRIVE/Ubuntu/Pictures/2015')
+        c.folders_to_exclude = ['2015']
+        c.useExif = False
+        c.extensions_to_ignore.extend(['.gif','.wav', '.png', '.bmp', '.db', '.tmp', '.ini', '.mix', '.mts', 'doc', '.tif'])
+        c.compare(r'/mnt/E_DRIVE/GoogleDrive/Google Photos', r'/mnt/E_DRIVE/Ubuntu/Pictures')
 
     except Exception as e:
         print("Exception =", e)
